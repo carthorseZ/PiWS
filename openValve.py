25d43e6440f02dacbc2eef3d3cc8abf06570ffae
David Cartman <cartman.david@gmail.com>
2020-12-20 09:39:03 +1300

    Create openValve.py

---
+++ openValve.py
@@ -0,0 +1,31 @@
+import RPi.GPIO as GPIO
+import time
+import sys
+import datetime
+import os
+import config
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(15,GPIO.OUT)
+                    
+if len(sys.argv) > 1:
+   wateringTime = float(sys.argv[1]) * 60.0
+else:
+    wateringTime = 60
+    
+print("Watering Starts ", datetime.datetime.now())
+for email in config.notificationList:
+  stream = os.popen("echo 'I have started watering your garden' | msmtp " + email)
+  print("email message :" + stream.read())
+
+GPIO.output(15,GPIO.HIGH)
+
+time.sleep(wateringTime)
+
+GPIO.output(15,GPIO.LOW)
+GPIO.cleanup()
+                    
+print("Watering Ends ", datetime.datetime.now())
+for email in config.notificationList:
+  stream = os.popen("echo 'I have finished watering your garden' | msmtp " + sendTo)
+  print("email message :" + stream.read())
